c4d245d2e85c0b384d38b28864869d69094670cc
Talyslan Canabarro <78499700+Talyslan@users.noreply.github.com>
2024-10-06 21:20:54 -0300

    fix: remove className unused

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -22,10 +22,10 @@ export function Button({
             onClick={onClick}
             type={type}
             disabled={disabled}
-            className={`lg:px-12 px-4 py-2 lg:text-2xl text-xl rounded-md uppercase font-crisis transition-all duration-150
+            className={`lg:px-12 px-4 py-2 lg:text-2xl text-xl rounded-md uppercase font-crisis transition-all duration-150 active:scale-[0.98]
                 ${isAnswer ? `shadow-Answer bg-white text-black` : 'shadow-NotAnswer bg-greenDark text-white hover:bg-greenDarker hover:scale-[1.01]'}
                 ${disabled ? '!text-gray-200 !bg-gray-400 !shadow-gray-500 !cursor-not-allowed hover:scale-[1]' : ''}
-                active:scale-[0.98]
+                ${className}
             `}
         >
             { children }
