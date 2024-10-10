955c0ae4bfb2f49d84f327e8fe11dab6c7bf4b75
Talyslan <talyslancpc@gmail.com>
2024-10-10 09:47:16 -0300

    fix & refactoring: confetti

--- src/app/(quiz)/quiz/components/quizGame.tsx
+++ src/app/(quiz)/quiz/components/quizGame.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 import { questions, IQuizGame } from './quizGame/types&array';
 import { useState } from 'react';
 import { Button } from '@/components/Button';
-import confetti from 'canvas-confetti';
+import { conffetisExplosion } from '../utils/confetti';
 
 export function QuizGame({ name }: IQuizGame) {
     const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -54,49 +54,6 @@ export function QuizGame({ name }: IQuizGame) {
         }
         return ''; 
     };
-
-    var count = 200;
-    var defaults = { origin: { y: 0.7 } };
-
-    interface IOpts {
-        spread?: number;
-        decay?: number;
-        scalar?: number;
-        startVelocity?: number;
-    } 
-
-    function fire(particleRatio: number, opts: IOpts) {
-        confetti({
-            ...defaults,
-            ...opts,
-            particleCount: Math.floor(count * particleRatio)
-        });
-    }
-
-    const conffetisExplosion = () => {
-        fire(0.25, {
-            spread: 26,
-            startVelocity: 55,
-            });
-            fire(0.2, {
-            spread: 60,
-            });
-            fire(0.35, {
-            spread: 100,
-            decay: 0.91,
-            scalar: 0.8
-            });
-            fire(0.1, {
-            spread: 120,
-            startVelocity: 25,
-            decay: 0.92,
-            scalar: 1.2
-            });
-            fire(0.1, {
-            spread: 120,
-            startVelocity: 45,
-            });
-    }
     
     return (
         <>
